003576cadeeca3983d4ac069bb0c559e41d0d379
Marco Orellana <mafeorellana09@gmail.com>
2024-07-26 18:47:00 -0400

    first commit

---
+++ src/main/java/com/marco/usersmanagementsystem/dto/ReqRes.java
@@ -0,0 +1,30 @@
+package com.marco.usersmanagementsystem.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.marco.usersmanagementsystem.entity.OurUsers;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ReqRes {
+
+    private int statusCode;
+    private String error;
+    private String message;
+    private String token;
+    private String refreshToken;
+    private String expirationTime;
+    private String nombre;
+    private String apellido;
+    private String ciudad;
+    private String role;
+    private String email;
+    private String password;
+    private OurUsers ourUsers;
+    private List<OurUsers> ourUsersList;
+
+}
